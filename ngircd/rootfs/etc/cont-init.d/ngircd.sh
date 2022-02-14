33abd9ba5b4bf85c993aa54c9bcc06cb2a735e68
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2022-02-14 17:03:56 +0100

    Get config from /config

--- ngircd/rootfs/etc/cont-init.d/ngircd.sh
+++ ngircd/rootfs/etc/cont-init.d/ngircd.sh
@@ -5,4 +5,7 @@
 
 # shellcheck shell=bash
 
-bashio::log.info "configure here!"
+bashio::log.info "Configuring NgIRCd..."
+
+rsync -a -v --ignore-existing /etc/ngircd/ngircd.conf /config/ngircd.conf || bashio::exit.nok 'Failed to make example config.' # Doesn't overwrite
+cp -a -f /config/ngircd.conf /etc/ngircd/ngircd.conf || bashio::exit.nok 'Failed to get ngircd.conf from /config.' # Does overwrite
